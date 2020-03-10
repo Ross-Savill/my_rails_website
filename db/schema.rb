271d3216eb22ad1f8c618971b348be85ada54756
Ross Savill <rosssavill252@hotmail.com>
2020-03-10 15:51:51 +0000

    active record storage tables added for blog images

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200310141844) do
+ActiveRecord::Schema.define(version: 2020_03_10_152402) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "blogs", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
+    t.string "title"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "qualifications", force: :cascade do |t|
-    t.string   "topic"
-    t.string   "level"
-    t.string   "grade"
+    t.string "topic"
+    t.string "level"
+    t.string "grade"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "works", force: :cascade do |t|
-    t.string   "organisation"
-    t.string   "role"
-    t.string   "years"
-    t.text     "tasks"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string "organisation"
+    t.string "role"
+    t.string "years"
+    t.text "tasks"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
