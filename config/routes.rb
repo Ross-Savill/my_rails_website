1ef497a814e949746d133b92baa1b3dbce3a2968
Ross Savill <rosssavill252@hotmail.com>
2020-03-12 23:58:56 +0000

    css adds

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :blogs, :works, :qualifications, :homepage_items
+  resources :blogs, :works, :qualifications
+  resources :homepage_items, only: [:edit, :update, :index]
   root 'homepage_items#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
