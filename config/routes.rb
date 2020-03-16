f5ef1ef95366e63d30fd15753a616a21e03b32f9
Ross Savill <rosssavill252@hotmail.com>
2020-03-16 01:30:26 +0000

    basic admin sign in established

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'homepage_items#index'
+
   resources :admins
   resources :blogs, :works, :qualifications
   resources :homepage_items, only: [:edit, :update, :index]
-  root 'homepage_items#index'
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get "signup", to: "admins#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
