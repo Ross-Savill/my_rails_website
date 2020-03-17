38197a486a740952d85c039c735aa552e25d4c9f
Ross Savill <rosssavill252@hotmail.com>
2020-03-17 23:06:15 +0000

    added qualification form

--- app/controllers/qualifications_controller.rb
+++ app/controllers/qualifications_controller.rb
@@ -4,6 +4,18 @@ class QualificationsController < ApplicationController
   end
 
   def create
+    @qualification = Qualification.new(qualification_params)
+
+    respond_to do |format|
+      if @qualification.save
+        format.html { redirect_to root_path, notice: 'Qualification was successfully created.' }
+        format.json { render :show, status: :created, location: @qualification }
+      else
+        format.html { render :new }
+        format.json { render json: @qualification.errors, status: :unprocessable_entity }
+      end
+    end
+
   end
 
   def edit
@@ -14,4 +26,14 @@ class QualificationsController < ApplicationController
 
   def delete
   end
+
+private
+
+def qualification_params
+  params.require(:qualification).permit(:topic, :level, :grade)
+
+  result =  params.require(:qualification).permit(:topic, :level, :grade)
+  result
+end
+
 end
