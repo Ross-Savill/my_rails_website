287eba57a85b5b0d492f0b216f2ad1e5c674a4ee
Ross Savill <rosssavill252@hotmail.com>
2020-04-06 23:01:39 +0100

    Establishing title and about us editing

--- app/controllers/homepage_items_controller.rb
+++ app/controllers/homepage_items_controller.rb
@@ -1,12 +1,13 @@
 class HomepageItemsController < ApplicationController
   def index
-    @homepage_data = HomePage.first
+    @homepage_dataset = HomePage.first
   end
 
   def new
   end
 
   def edit
+    @homepage_data = HomePage.find(1)
   end
 
   def create
@@ -16,5 +17,6 @@ class HomepageItemsController < ApplicationController
   end
 
   def update
+    @homepage_data = HomePage.find(1)
   end
 end
